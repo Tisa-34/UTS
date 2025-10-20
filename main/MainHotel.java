651ea49b0af22eeef1ba64dea23474c21af6d73f
Tisa-34 <bliissty73@gmail.com>
2025-10-20 16:47:44 +0700

    modifikasi file Service dan Main

--- main/MainHotel.java
+++ main/MainHotel.java
@@ -1,4 +1,98 @@
+package Main;
+//public class MainHotel {}
+import model.*;
+import Service.*;
+import java.util.Scanner;
 
 public class MainHotel {
+    private static HotelService hotelService = new HotelService();
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        boolean running = true;
+        while (running) {
+            tampilkanMenuUtama();
+            System.out.print("Pilih Menu: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    tampilkanKamar();
+                    break;
+                case "2":
+                    prosesReservasi();
+                    break;
+                case "3":
+                    tampilkanRaservasi();
+                    break;
+                case "4":
+                    prosesCheckOut();
+                    break;
+                case "5":
+                    running = false;
+                    System.out.println("\nTerima kasih telah menggunakan Aplikasi Reservasi Hotel. Sampai jumpa!");
+                    break;
+                default:
+                    System.out.println("\nPilihan tidak valid. Silakan coba lagi.");
+            }
+            System.out.println("\n");
+        }
+        scanner.close();
+    }
+
+    private static void tampilkanMenuUtama() {
+        System.out.println("========================================");
+        System.out.println("  APLIKASI RESERVASI HOTEL (CONSOLE)  ");
+        System.out.println("========================================");
+        System.out.println("1. Tampilkan Daftar Kamar");
+        System.out.println("2. Buat Reservasi Baru");
+        System.out.println("3. Lihat Daftar Reservasi Aktif");
+        System.out.println("4. Proses Check-Out");
+        System.out.println("5. Keluar");
+        System.out.println("========================================");
+    }
     
+    private static void tampilkanKamar() {
+        hotelService.tampilkanSemuaKamar();
+    }
+
+    private static void prosesReservasi() {
+        System.out.println("\n--- FORM RESERVASI ---");
+        System.out.print("Masukkan NIK Tamu: ");
+        String nik = scanner.nextLine();
+        System.out.print("Masukkan Nama Tamu: ");
+        String nama = scanner.nextLine();
+        System.out.print("Masukkan No. Telepon: ");
+        String telepon = scanner.nextLine();
+        Tamu tamuBaru = new Tamu(nik, nama, telepon);
+
+        System.out.println("\nTersedia Tipe Kamar: Standar, Deluxe, Suite");
+        System.out.print("Pilih Tipe Kamar: ");
+        String tipe = scanner.nextLine();
+        
+        int jumlahMalam = -1;
+        while(jumlahMalam <= 0) {
+            System.out.print("Lama Menginap (malam): ");
+            try {
+                jumlahMalam = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Input harus berupa angka.");
+            }
+        }
+
+        hotelService.buatRaservasi(tamuBaru, tipe, jumlahMalam);
+    }
+
+    private static void tampilkanRaservasi() {
+        hotelService.tampilkanSemuaRaservasi();
+    }
+
+    private static void prosesCheckOut() {
+        System.out.println("\n--- FORM CHECK-OUT ---");
+        System.out.print("Masukkan Kode Reservasi (cth: RES1): ");
+        String kode = scanner.nextLine();
+        hotelService.checkOut(kode);
+    }
 }
+
+
--- service/HotelService.java
+++ service/HotelService.java
@@ -1,5 +1,97 @@
+package Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Kamar;
+import model.KamarDeluxe;
+import model.KamarStandar;
+import model.KamarSuite;
+import model.Raservasi;
+import model.Tamu;
 
 public class HotelService {
+    private List<Kamar> daftarKamar;
+    private List<Raservasi> daftarRaservasi;
+
+    public HotelService() {
+        daftarKamar = new ArrayList<>();
+        daftarRaservasi = new ArrayList<>();
+        inisialisasiKamar();
+    }
+
+    private void inisialisasiKamar() {
+        daftarKamar.add(new KamarStandar("101"));
+        daftarKamar.add(new KamarStandar("102"));
+        daftarKamar.add(new KamarDeluxe("201"));
+        daftarKamar.add(new KamarDeluxe("202"));
+        daftarKamar.add(new KamarSuite("301"));
+
+    }
+
+    public void tampilkanSemuaKamar(){
+        System.out.println("\n--- DAFTAR KAMAR HOTEL ---");
+        for (Kamar kamar : daftarKamar) {
+            System.out.println(kamar.toString() + " | " + kamar.getDetailFasilitas() );
+        }
+    }
+
+    /** 
+   * @param 
+   * @return
+    */
+
+    public Kamar cariKamarKosong(String tipe) {
+        for (Kamar kamar : daftarKamar) {
+            if (kamar.getTipeKamar().equalsIgnoreCase(tipe) && kamar.getStatus().equals("Kosong")) {
+                return kamar;
+            }
+        }
+        return null;
+    }
+    public boolean buatRaservasi(Tamu tamu, String tipeKamar, int jumlahMalam) {
+        Kamar kamarTersedia = cariKamarKosong(tipeKamar);
+
+        if (kamarTersedia != null) {
+            String kode = "RES" + (daftarRaservasi.size() + 1);
+            Raservasi raservasiBaru = new Raservasi(kode, tamu, kamarTersedia, jumlahMalam);
+            kamarTersedia.setStatus("Terisi");
+            daftarRaservasi.add(raservasiBaru);
+            System.out.println("\n✅ RESERVASI BERHASIL DIBUAT!");
+            System.out.println(raservasiBaru.getDetailRaservasi());
+            return true;
+            
+        } else {
+            System.out.println("\n❌ RESERVASI GAGAL! Kamar tipe " + tipeKamar + " sedang Penuh.");
+            return false;
+        }
+    }
+    public void tampilkanSemuaRaservasi() {
+        if (daftarRaservasi.isEmpty()) {
+            System.out.println("\nTidak ada reservasi aktif saat ini.");
+            return;
+            
+        }
+        System.out.println("\n--- DAFTAR RESERVASI AKTIF ---");
+        for (Raservasi res : daftarRaservasi) {
+            System.out.println("================================");
+            System.out.println(res.getDetailRaservasi());
+        }
+    }
+    public boolean checkOut(String kodeRaservasi) {
+        for (Raservasi res : daftarRaservasi){
+            if (res.getKodeRaservasi().equalsIgnoreCase(kodeRaservasi) && res.getStatusPembayaran().equals("Belum Lunas")) {
+                res.setStatusPembayaran("LUNAS");
+                res.getKamar().setStatus("Kosong");
+                System.out.println("\n✅ CHECK-OUT BERHASIL!");
+                System.out.println("Kamar " + res.getKamar().getNomorKamar() + " telah dikosongkan.");
+                System.out.println("Total Biaya: Rp " + String.format("%.0f", res.getTotalBiaya()) + " (LUNAS)");
+                return true;
+                
+            }
+        }
+        System.out.println("\n❌ CHECK-OUT GAGAL! Kode reservasi tidak ditemukan atau sudah lunas.");
+        return false;
+    }
     
 }
